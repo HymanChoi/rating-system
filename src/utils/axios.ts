859d9bce488066501be451f9ae49d3d57a0d7c5b
HymanChoi <HymanChoi@outlook.com>
2023-06-15 09:52:53 +0800

    1. 更新请求配置问题；

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,15 +3,13 @@ import axios from "axios";
 // 创建一个实例
 const service = axios.create({
   baseURL: "",
-  timeout: 1000,
+  timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
 
 // 添加请求拦截器
 service.interceptors.request.use(
   (config) => {
-    // 添加apikey
-    config.params.apikey = "0df993c66c0c636e29ecbb5344252a4a";
     return config;
   },
   (error) => {
